397fb3a64af185ff0e8ee5ee0e7b04216bd12837
viveretur <191765834+viveretur@users.noreply.github.com>
2026-06-29 21:35:50 +0800

    Fleshed out a lot of the ISA/ABI.

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -20,5 +20,5 @@ build_doc() {
 
 build_doc "docs/architecture" "architecture.tex"
 # build_doc "docs/platform" "platform.tex"
-# build_doc "docs/abi" "abi.tex"
+build_doc "docs/abi" "abi.tex"
 # build_doc "docs/toolchain" "toolchain.tex"
