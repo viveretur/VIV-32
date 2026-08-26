bc6868942289c2f17d4aaf4e1a3ecf6ea497f7a2
viveretur <191765834+viveretur@users.noreply.github.com>
2026-08-26 18:52:14 +0800

    Updated screenshot to use demos/echo, showing ISR

--- demos/echo/main.asm
+++ demos/echo/main.asm
@@ -1,36 +1,32 @@
-; Demo program for testing assembler.
+; Demo echo program for testing assembler
+; Duplicates input upon buffer flush
 
-; Shared definitions from serial_tx.asm and serial spec.
+; Shared definitions from serial_tx.asm and serial spec
 .nomangle serial_set_control
 .nomangle serial_has_byte
 .nomangle serial_read
 .nomangle serial_send
-.const SERIAL_DUPLEX    0x17            ; Enabled for duplex transmission.
-.const SERIAL_TX_READY  0x2             ; Mask for testing
+.const SERIAL_DUPLEX    0x17            ; Enables duplex transmission and ISR
 
-; Data stored in interrupt routines
-.nomangle serial_buffer
-.nomangle serial_buffer_len
-
-.nomangle _main
+.nomangle _main                         ; Called by the reset vector at 0x00
 _main:
     li      %sp, 8192                   ; Initialise the stack pointer
-    lli     $1, SERIAL_DUPLEX
+    lli     $1, SERIAL_DUPLEX           ; Sets argument for serial control
     call    serial_set_control
-    ei
+    ei                                  ; Enable interrupts after enabling ISR
 
 loop:
     call    serial_has_byte
-    b.eq    $2, $0, loop
+    b.eq    $2, $0, loop                ; Loop until a byte is recieved
 
     call    serial_read
-    b.eq    $2, $0, _end
-    mov     $1, $2
+    b.eq    $2, $0, _end                ; Check for EOF
+    mov     $1, $2                      ; Prepare to send byte
 
 send_loop:
     call    serial_send
     b.ne    $2, $0, send_loop           ; Serial could not send, retry
-    jmp     loop    
+    jmp     loop
     
 _end:
     halt
--- demos/lib/serial_tx.asm
+++ demos/lib/serial_tx.asm
@@ -16,88 +16,85 @@
 
 ; Set value, from $1
 serial_set_control:
-    di
-    push    $2
-    
-    li      $2, SERIAL_CONTROL
-    sw      $1, [$2, 0]
-
-    pop     $2
-    ei
+    push    $2                          ; Protect used register
+    li      $2, SERIAL_CONTROL          ; Load the control's MMIO address
+    sw      $1, [$2, 0]                 ; Send the configuration via MMIO
+    pop     $2                          ; Restore used register
     ret
 
 ; Retrieve the current status, into $2
 serial_status:
-    li      $2, SERIAL_STATUS
-    lw      $2, [$2, 0]
+    li      $2, SERIAL_STATUS           ; Load the status's MMIO address
+    lw      $2, [$2, 0]                 ; Read it into the return register
     ret
 
 ; Returns 0 if no data, other value if not, into $2
 serial_has_byte:
-    push    $1, $3
-    la      $1, serial_buffer_head
-    lb      $1, [$1, 0]
-    la      $2, serial_buffer_tail
-    lb      $2, [$2, 0]
-    sub     $2, $2, $1
-    pop     $3, $1
+    push    $1, $3                      ; Protect used registers
+    la      $1, serial_buffer_head      ; Load the ring buffer head pointer
+    lb      $1, [$1, 0]                 ; Get its offset
+    la      $2, serial_buffer_tail      ; Load the ring buffer tail pointer
+    lb      $2, [$2, 0]                 ; Get its offset
+    sub     $2, $2, $1                  ; Put the difference in return register
+    pop     $3, $1                      ; Restore protected registers
     ret
  
 ; Retrieve the oldest byte in the buffer, into $2. If there is none, reads 0.
 serial_read:
-    push    %lr
-    call    serial_has_byte
-    pop     %lr
-    b.eq    $2, $0, serial_read_end
+    push    %lr                         ; Protect own call return register
+    call    serial_has_byte             ; because this call will wipe it
+    pop     %lr                         ; Restore after call
+    b.eq    $2, $0, serial_read_end     ; No data, so go to return
 
-    push    $1, $3, $4
-    la      $1, serial_buffer
-    la      $3, serial_buffer_tail
-    lb      $4, [$3, 0]
-    add     $1, $1, $4
-    lb      $2, [$1, 0]
-    inc     $4
-    sb      $4, [$3, 0]
-    pop     $4, $3, $1
+    push    $1, $3, $4                  ; Protect used registers
+    la      $1, serial_buffer           ; $1 holds the ring buffer base address
+    la      $3, serial_buffer_tail      ; $3 holds pointer to tail offset
+    lb      $4, [$3, 0]                 ; Read tail offset value into $4
+    add     $1, $1, $4                  ; Add ring base + offset for tail locn
+    lb      $2, [$1, 0]                 ; Read tail byte into return reg
+    inc     $4                          ; Increment tail offset value
+    sb      $4, [$3, 0]                 ; Save tail offset value back, wrapped
+    pop     $4, $3, $1                  ; Restore used registers
 
 serial_read_end:
     ret
 
-; Send the current byte. Sets $2 = 0 if successful, $2 = 1 if not.
+; Send the byte in $1. Sets $2 = 0 if successful, $2 = 1 if not.
 serial_send:
-    li      $2, SERIAL_STATUS
-    lw      $2, [$2, 0]
-    andi    $2, $2, STATUS_TX_READY
-    b.eq    $2, $0, ser_not_ready       ; SERIAL_TX is high if ready
+    li      $2, SERIAL_STATUS           ; Load MMIO address of status
+    lw      $2, [$2, 0]                 ; Read the status
+    andi    $2, $2, STATUS_TX_READY     ; Check if its ready, TX_READY is 1
+    b.eq    $2, $0, ser_not_ready       ; Skip send if serial tx not ready
 
-    li      $2, SERIAL_DATA
-    sb      $1, [$2, 0]
-    clr     $2
+    li      $2, SERIAL_DATA             ; Load MMIO address of serial data byte
+    sb      $1, [$2, 0]                 ; Write the byte, triggering transmit
+    clr     $2                          ; Clear for success value return
     ret
 
 ser_not_ready:
-    li      $2, 1
-    ret   
+    li      $2, 1                       ; Load error value into return
+    ret
 
+; Interrupt handler. Loads received byte into ring buffer.
 serial_isr:
-    push    $1, $2, $3, $4, %lr
+    push    $1, $2, $3, $4, %lr         ; Protect used registers
 
-    call    serial_status
-    andi    $2, $2, STATUS_RX_READY
-    b.eq    $2, $0, serial_isr_end
+    call    serial_status               ; Read status register, check for recv
+    andi    $2, $2, STATUS_RX_READY     ; RX_READY is 1 if there's data to read
+    b.eq    $2, $0, serial_isr_end      ; If no data, return is 0, skip to end
     
-    li      $1, SERIAL_DATA
-    lb      $1, [$1, 0]
-    la      $2, serial_buffer
-    la      $3, serial_buffer_head
-    lb      $4, [$3, 0]
-    add     $2, $2, $4
-    sb      $1, [$2, 0]
-    inc     $4
-    sb      $4, [$3, 0]
+    li      $1, SERIAL_DATA             ; Load MMIO address of serial data byte
+    lb      $1, [$1, 0]                 ; Read the byte
+    la      $2, serial_buffer           ; Load address of ring buffer into $2
+    la      $3, serial_buffer_head      ; Load address of buffer head into $3
+    lb      $4, [$3, 0]                 ; Read head offset into $4
+    add     $2, $2, $4                  ; Calculate base+offset into $2
+    sb      $1, [$2, 0]                 ; Store the read byte to base+offset $2
+    inc     $4                          ; Increment the offset value by 1
+    sb      $4, [$3, 0]                 ; Write the offset value back, wrapping
 
 serial_isr_end:
-    pop     %lr, $4, $3, $2, $1
+    pop     %lr, $4, $3, $2, $1         ; Restore used registers
     ret
 
 .data
